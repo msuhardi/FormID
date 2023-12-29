451d7fbe2247acb5c3c73383971e69c2d1799810
Maria Angela Suhardi <2810139+msuhardi@users.noreply.github.com>
2023-12-29 10:16:54 +0800

    chore: change null default to ''

--- src/app/config/features/sms.config.ts
+++ src/app/config/features/sms.config.ts
@@ -12,25 +12,25 @@ const smsSchema: Schema<ISms> = {
   twilioAccountSid: {
     doc: 'Twilio messaging ID',
     format: String,
-    default: null,
+    default: '',
     env: 'TWILIO_ACCOUNT_SID',
   },
   twilioApiKey: {
     doc: 'Twilio standard API Key',
     format: String,
-    default: null,
+    default: '',
     env: 'TWILIO_API_KEY',
   },
   twilioApiSecret: {
     doc: 'Twilio API Secret',
     format: String,
-    default: null,
+    default: '',
     env: 'TWILIO_API_SECRET',
   },
   twilioMsgSrvcSid: {
     doc: 'Messaging service ID',
     format: String,
-    default: null,
+    default: '',
     env: 'TWILIO_MESSAGING_SERVICE_SID',
   },
   smsVerificationLimit: {
